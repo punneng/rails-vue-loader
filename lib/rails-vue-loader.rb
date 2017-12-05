3922a1aae34a877d6734ef55e221415da78adf07
Surasit Liangpornrattana <punneng@gmail.com>
2017-12-05 16:01:28 +0700

    Add allowed js extension in vue

--- lib/rails-vue-loader.rb
+++ lib/rails-vue-loader.rb
@@ -5,7 +5,8 @@ require 'sprockets/vue/script'
 require 'sprockets/vue/style'
 module Sprockets
   if respond_to?(:register_transformer)
-    register_mime_type 'text/vue', extensions: ['.vue'], charset: :unicode
+    extensions = ['.vue'].concat(Sprockets::Vue::Script::ALLOWED_JS_EXTS)
+    register_mime_type 'text/vue', extensions: extensions, charset: :unicode
     register_transformer 'text/vue', 'application/javascript', Vue::Script
     register_transformer 'text/vue', 'text/css', Vue::Style
 
